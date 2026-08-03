4d18dc5d8222c1d063de4c5b4b1ee1e8ba1b130e
Suman Saha <8759127658itisaha@gmail.com>
2026-08-03 23:56:29 +0530

    types of attributes and methods

--- index.py
+++ index.py
@@ -42,16 +42,46 @@
 # print(s1.name)
 # print(s2.age)
 
-class Bags:
-    def __init__(self, material, zips, color, pockets):
-        self.material = material
-        self.zips = zips
-        self.color = color
-        self.pockets = pockets
+# class Bags:
+#     def __init__(self, material, zips, color, pockets):
+#         self.material = material
+#         self.zips = zips
+#         self.color = color
+#         self.pockets = pockets
 
-reebok = Bags("leather", 2, "black", 3)
-print(reebok.material, reebok.zips, reebok.color, reebok.pockets)
+# reebok = Bags("leather", 2, "black", 3)
+# print(reebok.material, reebok.zips, reebok.color, reebok.pockets)
 
-campus = Bags("canvas", 1, "blue", 2)
-print(campus.material, campus.zips, campus.color, campus.pockets)
+# campus = Bags("canvas", 1, "blue", 2)
+# print(campus.material, campus.zips, campus.color, campus.pockets)
+
+
+# Types of Atributes and methods
+
+class Animal:
+    a = 10 # class attribute
+
+    def __init__(self, name, age):
+        self.name = name # instance attribute
+        self.age = age # instance attribute
+
+    def details(self): # instance method
+        print(f"Name: {self.name}, Age: {self.age}")
+
+    @classmethod
+    def hello(cls):
+        print("Hello, I am an animal") # class method
+
+    @staticmethod
+    def info():
+        print("Animals are multicellular eukaryotic organisms") # static method    
+
+obj = Animal("Dog", 5)
+
+print(obj.a) # accessing class attribute using object
+print(obj.name) # accessing instance attribute using object
+print(obj.age) # accessing instance attribute using object
+
+obj.details() # calling instance method using object
+
 
