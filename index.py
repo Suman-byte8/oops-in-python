c175fefca2c879457066c3f0f9eeca0a250a43f5
Suman Saha <8759127658itisaha@gmail.com>
2026-08-03 22:36:17 +0530

    objects

--- index.py
+++ index.py
@@ -1,11 +1,25 @@
 # class Car:
 #     print("hello how are you")
 
-class Car:
-    a = 10 #attribute
+# class Car:
+#     a = 10 #attribute
 
-    def hello(): #method
-        print("hello how are you")
+#     def hello(): #method
+#         print("hello how are you")
 
-print(Car.a)
-Car.hello()
+# print(Car.a)
+# Car.hello()
+
+# objects
+class Bags:
+    name = "Gucci"
+    def details(self):
+        print("This is a Gucci bag")
+
+reebok = Bags() # creating an object (reebok) of class Bags
+campus = Bags() # creating an object (campus) of class Bags
+
+campus.name = "Campus" # changing the attribute name of object campus
+
+print(campus.name) # printing the attribute name of object campus
+reebok.details() # calling the method details() using the object reebok
