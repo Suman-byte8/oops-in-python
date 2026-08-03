38fc9a7f23ee83637946fa93a347b875accb8d92
Suman Saha <8759127658itisaha@gmail.com>
2026-08-03 22:17:03 +0530

    attributes and methods in class

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+# class Car:
+#     print("hello how are you")
+
+class Car:
+    a = 10 #attribute
+
+    def hello(): #method
+        print("hello how are you")
+
+print(Car.a)
+Car.hello()
